7d733625a973ce1428d7473026880b196877d81b
Xavier Beheydt <xavier.beheydt@gmail.com>
2024-02-06 02:27:27 +0100

    Add sdl2 ttf package

--- cmake/extern/sdl2.cmake
+++ cmake/extern/sdl2.cmake
@@ -2,6 +2,6 @@
 
 find_package(SDL2 CONFIG REQUIRED)
 find_package(SDL2_image CONFIG REQUIRED)
-# find_package(SDL2_ttf CONFIG REQUIRED)
+find_package(SDL2_ttf CONFIG REQUIRED)
 # find_package(SDL2_mixer CONFIG REQUIRED)
 # find_package(SDL2_net CONFIG REQUIRED)
